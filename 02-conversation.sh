603876211e25de7447621c47b4d7e9520d7d0f9d
naveen21kumar95 <naveen21kumar95@gmail.com>
2025-06-17 21:02:19 -0400

    script 3 addition

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Trump:: Heyy Musk, How are you?"
+echo "Musk:: I am fine, Mr. President. How are you?"
+echo "Trump:: I am fine too. Thanks for asking."
+echo "Musk:: I am a big fan of yours."
+echo "Trump:: Thanks, Musk. I appreciate your support."
---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+PERSON1=Trump
+PERSON2=Musk
+
+echo "$PERSON1:: Heyy $PERSON2, How are you?"
+echo "$PERSON2:: I am fine, Mr. President. How are you?"
+echo "$PERSON1:: I am fine too. Thanks for asking."
+echo "$PERSON2:: I am a big fan of yours."
+echo "$PERSON1:: Thanks, $PERSON2. I appreciate your support."
