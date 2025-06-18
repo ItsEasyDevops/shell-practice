7106a801e27d5621361244baf55842398e68758b
naveen21kumar95 <naveen21kumar95@gmail.com>
2025-06-17 20:58:42 -0400

    Hello World change

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-echo "Trump:: Heyy Musk, How are you?"
-echo "Musk:: I am fine, Mr. President. How are you?"
-echo "Trump:: I am fine too. Thanks for asking."
-echo "Musk:: I am a big fan of yours."
-echo "Trump:: Thanks, Musk. I appreciate your support."
+echo "Hello, World!"
