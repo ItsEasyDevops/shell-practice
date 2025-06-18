5833f138284c7bff98e433ad2dc5b822d0a063da
naveen21kumar95 <naveen21kumar95@gmail.com>
2025-06-17 20:54:42 -0400

    Trump & Musk conversation

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "Hello World!"
+echo "Trump:: Heyy Musk, How are you?"
+echo "Musk:: I am fine, Mr. President. How are you?"
+echo "Trump:: I am fine too. Thanks for asking."
+echo "Musk:: I am a big fan of yours."
+echo "Trump:: Thanks, Musk. I appreciate your support."
