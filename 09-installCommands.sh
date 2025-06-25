c467a15bd86a7543c726421850ded83d747a30d6
naveen21kumar95 <naveen21kumar95@gmail.com>
2025-06-24 20:47:01 -0400

    09-installCommands.sh update

---
+++ 09-installCommands.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+USER=$(id u)
+
+if [ $USER -ne 0 ]
+then
+echo "Error! Please run this script using root access"
+exit 1 #Giving status anything other than 0 upto 127
+else
+echo "You're running with root access."
+fi
