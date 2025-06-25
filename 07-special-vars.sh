f45e27dd31a462b8b4049ab3d5cc64b8176ee72e
naveen21kumar95 <naveen21kumar95@gmail.com>
2025-06-24 20:19:51 -0400

    07-special-vars.sh file update

--- 07-special-vars.sh
+++ 07-special-vars.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 echo "All variables passed to the script: $*"
-echo "All variables passed to the script (as an array): $@" 
+echo "All variables passed to the script (as an array): $@"
+echo "Number of arguments/variables passed to the script: $#"
+echo "The name of the script: $0"
+echo "The PID of the current script: $$"
+echo "The PID of the last executed command: $!"
+echo "Exit status of the last command: $?"
+echo "Home directory of the current user: $HOME"
+echo "Current working directory of the script: $PWD"
+echo "Current user: $USER"
+sleep 10 &
+echo "PID of the last background command: $!"
