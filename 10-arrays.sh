62c6560893444f124864047d1de2b106952c66be
naveen21kumar95 <naveen21kumar95@gmail.com>
2025-06-26 23:23:02 -0400

    10-arrays.sh file

---
+++ 10-arrays.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Below is an example of an array in bash
+PERSONS=("Trump","Musk","Zuckerberg","Bezos")
+
+#Here we are printing the first element of the array which starts from index 0
+echo "${PERSONS[0]}:: Heyy ${PERSONS[1]}, How are you?"
