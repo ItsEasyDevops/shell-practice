dc7c799f398de7c5f0dd5b0cc83016c776f3f9cd
naveen21kumar95 <naveen21kumar95@gmail.com>
2025-06-28 15:05:21 -0400

    Adding 11-functions.sh

---
+++ 11-functions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Defining a function called add numbers
+add_numbers() {
+   local num1=$1
+   local num2=$2
+
+#Perform Addition
+   local sum=$((num1+num2))
+
+#Output the result
+   echo "The sum of $num1 and $num2 is: $sum"
+}
+
+#call the function with arguments
+add_numbers 5 7
+
